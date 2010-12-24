db1ca6145eb6e768fcaaad1144b25d959997e79c
raeffu <raeffu@raeffu.ch>
2010-12-24 15:26:57 +0100

    new files

---
+++ calc.rb
@@ -0,0 +1,16 @@
+#time
+#time = {}
+
+#time["yearinmonth"] = 12
+#time["monthindays"] = 30
+#time["yearindays"] = 365
+#time["monthinweeks"] = 4
+#time["yearinweeks"] = 52
+#time["weekindays"] = 7
+#time["dayinhours"] = 24
+
+#hours per year
+#puts "One year has #{time["yearindays"]*time["dayinhours"]} hours."
+
+puts (((1031000000 / 3600) / 24) / 365)
+
